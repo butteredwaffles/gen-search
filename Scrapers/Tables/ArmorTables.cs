8f4225dfba4c57d3421ffdb04a180dd1217abcc8
ButteredWaffles <nobodywinds@gmail.com>
2018-06-30 10:59:25 -0400

    Add armor tables
    - Testing out one-to-many relationships, so things are not set in stone.

---
+++ Scrapers/Tables/ArmorTables.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+
+namespace Gensearch.Scrapers
+{
+    [Table("Armors")]
+    public class Armor {
+        [PrimaryKey, AutoIncrement]
+        public int armor_id {get; set;}
+        public string armor_set {get; set;}
+        public string armor_name {get; set;}
+        public string armor_description {get; set;}
+
+        public int min_armor_defense {get; set;}
+        public int max_armor_defense {get; set;}
+        public int fire_def {get; set;}
+        public int water_def {get; set;}
+        public int thunder_def {get; set;}
+        public int ice_def {get; set;}
+        public int dragon_def {get; set;}
+
+        public int slots {get; set;}
+        public int rarity {get; set;}
+        public int max_upgrade {get; set;}
+        [ForeignKey(typeof(Monster))]
+        public int monster_id {get; set;}
+
+        public string is_blademaster {get; set;}
+        public string is_gunner {get; set;}
+        public string is_male {get; set;}
+        public string is_female {get; set;}
+
+        [OneToMany(CascadeOperations = CascadeOperation.All)]
+        public List<ArmorSkill> skills {get; set;}
+    }
+
+    [Table("ArmorSkills")]
+    public class ArmorSkill {
+        [PrimaryKey, AutoIncrement]
+        public int as_id {get; set;}
+        [ForeignKey(typeof(Armor))]
+        public int armor_id {get; set;}
+        [ForeignKey(typeof(Skill))]
+        public int skill_id {get; set;}
+        public int skill_quantity {get; set;}
+    }
+
+
+}
