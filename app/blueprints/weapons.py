532b8aadab10876f379b330391070561bc39ee6c
ButteredWaffles <nobodywinds@gmail.com>
2018-07-28 20:56:14 -0400

    Add bow endpoint and fix one issue regarding elements
    Around 300 weapons' elements are still broken due to the issue described in GH-1.
    Fixed an issue where a weapon's element was being stored as a single string/int. Seeing as weapons can have multiple elements, and that weapon defense is also included in this table, this lead to some important things being left out...

--- app/blueprints/weapons.py
+++ app/blueprints/weapons.py
@@ -1,7 +1,60 @@
 from flask import Blueprint, jsonify, url_for
-from models.db_config import db
 from models.models import *
+from models.db_config import db
 
+
+BOW_COATINGS = {
+    "Pow1": {
+        "name": "Power Lvl1",
+        "stack_size": 50,
+        "effect": "1.35x raw"
+    },
+    "Pow2": {
+        "name": "Power Lvl2",
+        "stack_size": 50,
+        "effect": "1.50x raw"
+    },
+    "Ele1": {
+        "name": "Element Lvl1",
+        "stack_size": 20,
+        "effect": "1.35x element"
+    },
+    "Ele2": {
+        "name": "Element Lvl2",
+        "stack_size": 20,
+        "effect": "1.50x element"
+    },
+    "CRan": {
+        "name": "Close Range",
+        "stack_size": 20,
+        "effect": "Extends critical distance length"
+    },
+    "Psn": {
+        "name": "Poison",
+        "stack_size": 20,
+        "effect": "Applies poison element"
+    },
+    "Par": {
+        "name": "Paralysis",
+        "stack_size": 20,
+        "effect": "Applies paralysis element"
+    },
+    "Sle": {
+        "name": "Sleep",
+        "stack_size": 20,
+        "effect": "Applies sleep element"
+    },
+    "Exh": {
+        "name": "Exhaust",
+        "stack_size": 20,
+        "effect": "Applies exhaust and KO (if hit on head)"
+    },
+    "Bla": {
+        "name": "Blast",
+        "stack_size": 20,
+        "effect": "Applies blast element"
+    }
+}
 weapon_routes = Blueprint('weapons', __name__, template_folder="../templates", static_folder="../static")
 
 
@@ -19,6 +72,14 @@ def get_all_weapons_by_category(category):
                     "set": weapon.sword_set_name,
                     "class": weapon.sword_class,
                     "url": url_for(".get_weapon", _external=True, category=category, setname=weapon.sword_set_name)
+                })
+    elif category == "bow":
+        for weapon in Bow.select():
+            if weapon.bow_set_name not in found_sets:
+                found_sets.append(weapon.bow_set_name)
+                weapons["weapons"].append({
+                    "set": weapon.bow_set_name,
+                    "url": url_for(".get_weapon", _external=True, category=category, setname=weapon.bow_set_name)
                 })
     db.close()
     return jsonify(weapons)
@@ -41,21 +102,27 @@ def get_weapon(category, setname):
                 "description": wep.description,
                 "price": wep.price,
                 "monster": "none",
-                "element": "none",
-                "element_damage": 0,
+                "elements": [],
                 "upgrades_into": [
                     url_for('.get_weapon', _external=True, category="blademaster", setname=upgrade.replace(' 1', ""))
                     for upgrade in wep.upgrades_into.split(' & ') if upgrade != "none"
                 ],
                 "sharpness": {},
                 "crafting_materials": [],
-                "scraps": []
+                "scraps": [],
+                "defense": 0
             }
 
-            element = ElementDamage.select().where(ElementDamage.weapon_id == wep.sword_id)
-            if len(element) > 0:
-                weapon["element"] = element[0].elem_type
-                weapon["element_damage"] = element[0].elem_amount
+            elements = ElementDamage.select().where(ElementDamage.weapon_id == wep.sword_id)
+            if len(elements) > 0:
+                for element in elements:
+                    if "Def" == element.elem_type:
+                        weapon["defense"] = element.elem_amount
+                    else:
+                        weapon["elements"].append({
+                            "element": element.elem_type,
+                            "element_damage": element.elem_amount
+                        })
 
             if wep.monster_id != -1:
                 weapon["monster"] = Monster.get_by_id(wep.monster_id).mon_name
@@ -77,21 +144,60 @@ def get_weapon(category, setname):
 
             if wep.sword_class == "Hunting Horn":
                 weapon["notes"] = HuntingHorn.get(HuntingHorn.sword_id == wep.sword_id).notes
-            
+
             if wep.sword_class in ["Charge Blade", "Switch Axe", "Gunlance"]:
                 weapon["phial_shell"] = PhialAndShellWeapon.get(PhialAndShellWeapon.sword_id == wep.sword_id).phial_or_shell_type
 
-            for item in CraftItem.select().where(CraftItem.creation_type == "Blademaster", CraftItem.creation_id == wep.sword_id):
-                _item = {
-                    "name": item.item_name,
-                    "quantity": item.quantity,
-                    "unlocks_creation": True if item.unlocks_creation == "yes" else False,
-                }
-                if item.is_scrap == "no":
-                    weapon["crafting_materials"].append(_item)
-                else:
-                    weapon["scraps"].append(_item)
-            
+            weapon["crafting_materials"], weapon["scraps"] = get_crafting_items("Blademaster", wep.sword_id)
             weapons["weapons"].append(weapon)
+    elif category == "bow":
+        for db_bow in Bow.select().where(Bow.bow_set_name == setname):
+            bow = {
+                "name": db_bow.bow_name,
+                "damage": db_bow.bow_damage,
+                "affinity": db_bow.affinity,
+                "arc_type": db_bow.arc_type,
+                "charge_shots": [db_bow.level_one_charge, db_bow.level_two_charge, db_bow.level_three_charge, db_bow.level_four_charge],
+                "supported_coatings": [BOW_COATINGS[coating] for coating in db_bow.supported_coatings.split(', ')],
+                "slots": db_bow.slots,
+                "rarity": db_bow.rarity,
+                "description": db_bow.description,
+                "elements": [],
+                "defense": 0
+            }
+            bow["crafting_materials"], bow["scraps"] = get_crafting_items("Bow", db_bow.bow_id)
+
+            if db_bow.monster_id != -1:
+                bow["monster"] = Monster.get_by_id(db_bow.monster_id).mon_name
+
+            elements = ElementDamage.select().where(ElementDamage.weapon_id == db_bow.bow_id)
+            if len(elements) > 0:
+                for element in elements:
+                    if "Def" == element.elem_type:
+                        bow["defense"] = element.elem_amount
+                    else:
+                        bow["elements"].append({
+                            "element": element.elem_type,
+                            "element_damage": element.elem_amount
+                        })
+
+            weapons["weapons"].append(bow)
     db.close()
     return jsonify(weapons)
+
+
+def get_crafting_items(creation_type, weapon_id):
+    crafts = []
+    scraps = []
+    for item in CraftItem.select().where(CraftItem.creation_type == creation_type,
+                                         CraftItem.creation_id == weapon_id):
+        _item = {
+            "name": item.item_name,
+            "quantity": item.quantity,
+            "unlocks_creation": True if item.unlocks_creation == "yes" else False,
+        }
+        if item.is_scrap == "no":
+            crafts.append(_item)
+        else:
+            scraps.append(_item)
+    return crafts, scraps
